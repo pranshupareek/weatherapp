d7251452d9960837785b82f1c4f94822d7975801
Pranshu Pareek <pranshupareek47@gmail.com>
2020-05-05 00:35:29 +0530

    Added max and min temprature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (lat,lon,callback) => {
         } else if (body.cod!==200){
             callback('Unable to find location',undefined)
         } else {
-            callback(undefined, 'Current weather: '+body.weather[0].description+'. It is currently '+Math.floor(body.main.temp)/10+' degrees out'//{
+            callback(undefined, 'Current weather: '+body.weather[0].description+'. It is currently '+Math.floor(body.main.temp)/10+' degrees out. Maximum temperature is '+Math.floor(body.main.temp_min)/10+ ' degrees and Minimum is '+Math.floor(body.main.temp_min)/10+' degrees today.'//{
             //     weather: body.weather[0].description,
             //     temp: Math.floor(body.main.temp)/10
             // }
