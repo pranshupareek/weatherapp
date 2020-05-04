b1523576940ec4acca34b7c26f7d4937c1bccd3a
Pranshu Pareek <pranshupareek47@gmail.com>
2020-05-04 22:19:33 +0530

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('JS file loaded')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
